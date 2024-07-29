d366d7ad878ed3634e379161d3b44d6ecf77c432
ANSHU KUMAR SINGH <wwwanshusingh988@gmail.com>
2024-07-29 20:54:49 +0530

    Add files via upload

---
+++ anshu.c
@@ -0,0 +1,21 @@
+/*7. Write a program using function to print the following pattern (first n lines) 
+* 
+* * * 
+* * * * **/
+#include<stdio.h>
+int main(){
+    int n=3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; i < 2; j++)
+        {
+                printf("*");
+        }
+        printf("\n");
+        
+    }
+        return 0;
+    
+}
+    
+
